0ad97165550a54089639127e6dec7265cb183e6e
Aditya Singh <115808044+Adityadeveloper28@users.noreply.github.com>
2024-04-19 10:14:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb+srv://aditya:aditya123@cluster0.zoiqagj.mongodb.net/st
 });
 
 const marksSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: String,
   rollNo: String,
   MAD: Number,
   COA: Number,
